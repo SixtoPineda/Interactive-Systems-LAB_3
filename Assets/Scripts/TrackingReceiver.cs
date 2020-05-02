d3d3362f67959bd59400fd7aa2871ac2752a410b
SixtoPineda <63787315+SixtoPineda@users.noreply.github.com>
2020-05-02 13:52:29 +0200

    Add files via upload

---
+++ Assets/Scripts/TrackingReceiver.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using extOSC;
+using System.Linq;
+using System.Diagnostics;
+
+public class TrackingReceiver : MonoBehaviour
+{
+    //GameObjects to be controlled with Posenet
+    public GameObject nose; //Nariz
+    public GameObject wristR; //Mano derecha
+    public GameObject wristL; //Mano izq.
+    public GameObject background;
+
+    //OSC Variables
+    private OSCReceiver _receiver;
+    private const string _oscAddress = "/pose/0";
+    private player_control player_c;
+
+    //Dictionary to store pose data
+    public Dictionary<string, Vector3> pose = new Dictionary<string, Vector3>();
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        //Set up OSC receiver
+        StartOSCReceiver();
+
+        //Initialize pose
+        StartPose();
+    }
+
+    void StartOSCReceiver() {
+        // Creating a receiver.
+        _receiver = gameObject.AddComponent<OSCReceiver>();
+
+        // Set local port.
+        _receiver.LocalPort = 9876;
+
+        // Bind "MessageReceived" method to special address.
+        _receiver.Bind(_oscAddress, MessageReceived);
+    }
+
+    void StartPose() {//colocamos todos los objetos en 0
+        pose.Add("nose", Vector3.zero);
+        pose.Add("rightWrist", Vector3.zero);
+        pose.Add("leftWrist", Vector3.zero);
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        nose.transform.position = pose["nose"];
+        wristR.transform.position = pose["rightWrist"];
+        wristL.transform.position = pose["leftWrist"];
+        float XposL = wristL.transform.position.x;
+        float YposL = wristL.transform.position.y;
+
+        float XposR = wristR.transform.position.x;
+        float YposR = wristR.transform.position.y;
+
+        float r = ((XposL + XposR) / 2);
+        float g = ((YposL + YposR) / 2);
+        float b = (XposL + YposR) / 2;
+        if (r <= 0 && g<=0 && b<=0)
+        {
+            background.GetComponent<Renderer>().material.color = new Color(0.49f,0.22f,0.88f);
+        }
+        else
+        {
+            background.GetComponent<Renderer>().material.color = new Color(r, g, b); //cambiamos el color del fondo según el movimiento de nuestras manos
+        }
+
+        
+    }
+
+    protected void MessageReceived(OSCMessage message)
+    {
+        List<OSCValue> list = message.Values;
+        //UnityEngine.Debug.Log(list.Count);
+
+        for(int i=0;i<list.Count; i+=3)
+        {
+            string key = "";
+            Vector2 position = Vector3.zero; 
+
+            OSCValue val0 = list.ElementAt(i);
+            if (val0.Type == OSCValueType.String) key = val0.StringValue;
+            OSCValue val1 = list.ElementAt(i+1);
+            if (val1.Type == OSCValueType.Float) position.x = val1.FloatValue-250;
+            OSCValue val2 = list.ElementAt(i+2);
+            if (val2.Type == OSCValueType.Float) position.y = -(val2.FloatValue-250);
+
+            if (pose.ContainsKey(key)) {
+                pose[key] = position; 
+            }
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/player_control.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class player_control : MonoBehaviour
+{
+    public AudioClip pickup; //audio al recoger objeto
+    public AudioClip uwin; //audio al acabar el juego
+    public Text count_text; //texto de recuento de score
+    public Text win_text; //texto de que hemos ganado
+
+    public int count; //contador de SCORE
+
+    public GameObject cam;//cámara desde donde ve el objato
+    Vector3 pos_cam;
+
+
+    void Start()
+    {
+        count = 0; //puntos en un inicio 0
+        SetCount_text();//ploteamos texto
+        win_text.text = "";//texto de WIN vacío aun no hemos conseguido el objetivo
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Tag: para saber el nombre del Objeto
+        // SetActive: para activar (True) o no (False) un objeto, que aparezca o no, sin eliminar
+        // CompareTag: para comparar un Tag de un objeto con una string
+
+        if (other.gameObject.CompareTag("Pick_Up"))
+        {
+
+            pos_cam = cam.transform.position; //obtenemos la posicion de la camara para poner el sonido
+
+            AudioSource.PlayClipAtPoint(pickup, pos_cam); //PLAY audio al recoger un objeto
+            other.gameObject.SetActive(false); //hacemos desaparecer al objeto al recogerlo
+            count = count + 1;//auemnatamos los puntos +1
+            SetCount_text();//ploteamos los puntos actualizados
+        }
+        if (count >= 6)
+        {
+            AudioSource.PlayClipAtPoint(uwin, pos_cam); //si se recogen todos los cubos, PLAY música final
+            win_text.text = "¡GOOD JOB!";//TEXTo de que hemos ganado
+        }
+
+    }
+
+    void SetCount_text()//función para plotear el texto de los puntos SCORE
+    {
+        count_text.text = "SCORE: " + count.ToString();
+    }
+
+
+}
